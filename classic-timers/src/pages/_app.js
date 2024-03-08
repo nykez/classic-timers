3eb8b5e58c4f15aa1bb0ce4fc04f2cd03352dd4e
Nykez <nykezgaming@live.com>
2024-03-07 22:00:34 -0500

    set title

--- classic-timers/src/pages/_app.js
+++ classic-timers/src/pages/_app.js
@@ -1,10 +1,14 @@
 import Layout from "@/components/_layout";
 import "@/styles/globals.css";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
     return (
         <>
         <Layout>
+            <Head>
+                <title>Classic Timers</title>
+            </Head>
             <Component {...pageProps} />
         </Layout>
         </>
